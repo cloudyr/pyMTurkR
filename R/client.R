b9316a8e4b5db8dfea3c65420b2ab674067086b2
Tyler Burleigh <tylerburleigh@gmail.com>
2019-05-31 18:53:46 -0400

    Update client
    Client performs more like MTurkR, such that the user can set AWS keys as environment variables (instead of configuring a .aws/credentials file)
    
    Sys.setenv(AWS_ACCESS_KEY_ID = "my access key")
    Sys.setenv(AWS_SECRET_ACCESS_KEY = "my secret key")

--- R/client.R
+++ R/client.R
@@ -9,9 +9,9 @@
 #'
 #' @aliases GetClient StartClient Client client
 #' @param sandbox A logical indicating whether the client should be in the
-#' sandbox environment or the live environment.
+#' sandbox environment or the live environment. Set with
 #' @param profile A character string that specifies the profile to use
-#' from the .aws/credentials file.
+#' from the .aws/credentials file, optional.
 #' @return An object with the classes \dQuote{botocore.client.MTurk},
 #' \dQuote{botocore.client.BaseClient}, \dQuote{python.builtin.object}, that
 #' is used as an MTurk client
@@ -34,13 +34,15 @@ GetClient <-
 StartClient <-
 Client <-
 client <-
-function(sandbox = TRUE, profile = 'default'){
+function(sandbox = options('pyMTurkR.sandbox'), profile = options('pyMTurkR.profile')){
+
   tryCatch({ # Try loading boto3 module
     boto3 <- reticulate::import("boto3")
     .helper_mturk_client(sandbox, profile, boto3) # If the module loaded, start the client
   }, error = function(e) {
     message(paste(e, "Did you install boto3 for this python environment? Run py_config() to see the python environment you're in."))
   })
+
 }
 
 .helper_mturk_client <- function(sandbox, profile, boto3){
@@ -48,20 +50,29 @@ function(sandbox = TRUE, profile = 'default'){
   if(sandbox) endpoint_url = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com'
   else endpoint_url = 'https://mturk-requester.us-east-1.amazonaws.com'
 
-  # Check if AWS credentials exist for the profile
-  if(length(aws.signature::read_credentials()[[profile]]$AWS_ACCESS_KEY_ID) == 0 |
-     length(aws.signature::read_credentials()[[profile]]$AWS_SECRET_ACCESS_KEY) == 0)
-        message("ERROR: Missing AWS Access Key or Secret Access Key.\n  Make sure AWS is configured correctly.")
-
-  else { # If the credentials exist, load the client
-    client <- boto3$client('mturk', region_name='us-east-1',
-                                  aws_access_key_id = aws.signature::read_credentials()[[profile]]$AWS_ACCESS_KEY_ID,
-                                  aws_secret_access_key = aws.signature::read_credentials()[[profile]]$AWS_SECRET_ACCESS_KEY,
-                                  endpoint_url = endpoint_url)
-
-    # Now test the credentials
-    .helper_mturk_credentials_test(sandbox, client)
+  # Check if AWS credentials can be found in an environment variable
+  # else check .aws/credentials
+  # else stop
+  if(Sys.getenv("AWS_ACCESS_KEY_ID") != "" & Sys.getenv("AWS_SECRET_ACCESS_KEY") != ""){
+    key <- Sys.getenv("AWS_ACCESS_KEY_ID")
+    secret_key <- Sys.getenv("AWS_SECRET_ACCESS_KEY")
+  } else if (length(aws.signature::read_credentials()[[profile]]$AWS_ACCESS_KEY_ID) > 0 &
+              length(aws.signature::read_credentials()[[profile]]$AWS_SECRET_ACCESS_KEY) > 0) {
+    key <- aws.signature::read_credentials()[[profile]]$AWS_ACCESS_KEY_ID
+    secret_key <- aws.signature::read_credentials()[[profile]]$AWS_SECRET_ACCESS_KEY
+  } else {
+    stop("ERROR: Missing AWS Access Key or Secret Access Key.")
   }
+
+  # Start client
+  client <- boto3$client('mturk', region_name='us-east-1',
+                                aws_access_key_id = aws.signature::read_credentials()[[profile]]$AWS_ACCESS_KEY_ID,
+                                aws_secret_access_key = aws.signature::read_credentials()[[profile]]$AWS_SECRET_ACCESS_KEY,
+                                endpoint_url = endpoint_url)
+
+  # Test credentials with a simple API call
+  .helper_mturk_credentials_test(sandbox, client)
+
 }
 
 .helper_mturk_credentials_test <- function(sandbox, client){
