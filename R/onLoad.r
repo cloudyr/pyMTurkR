1e768273f63cad028c8e9e3922ab0cb8e613bd06
Tyler Burleigh <tylerburleigh@gmail.com>
2019-08-11 13:49:40 -0400

    Sandbox default to TRUE
    Sandbox default to TRUE

--- R/onLoad.r
+++ R/onLoad.r
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname){
     if (is.null(getOption("pyMTurkR.sandbox"))) {
-        options(pyMTurkR.sandbox = FALSE)     # sandbox logical
+        options(pyMTurkR.sandbox = TRUE)     # sandbox logical
     }
     if (is.null(getOption("pyMTurkR.verbose"))) {
         options(pyMTurkR.verbose = TRUE)      # print logical
