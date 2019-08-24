93a66aad43e2a22b56e99d1be560590a8863239c
Tyler Burleigh <tylerburleigh@gmail.com>
2019-08-24 15:29:23 -0400

    SearchQuals test
    SearchQuals test

--- tests/testthat/test-SearchQualificationTypes.R
+++ tests/testthat/test-SearchQualificationTypes.R
@@ -3,3 +3,13 @@ test_that("SearchQuals", {
   skip_if_not(CheckAWSKeys())
   expect_type(SearchQuals(verbose = FALSE), "list")
 })
+
+test_that("SearchQuals two results", {
+  skip_if_not(CheckAWSKeys())
+  SearchQuals(verbose = FALSE,
+              must.be.owner = TRUE,
+              results = 1,
+              return.pages = 2) -> result
+  expect_type(result, "list")
+  expect_length(result, 2)
+})
